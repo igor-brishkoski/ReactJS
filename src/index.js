a091cda5a0d9c80f1e362780e2fc36e4f0d0c818
Igor Brishkoski <ibrishkoski@wework.com>
2017-11-17 16:21:51 -0500

    Added some more js code

--- src/index.js
+++ src/index.js
@@ -4,19 +4,27 @@ import './index.css';
 
 function EventImage(props) {
   return (
-    <img class="eventImage" src={props.value} alt=""/>
+    <div class="eventImage" style={{backgroundImage: "url(" + props.value + ")"}}></div>
   )
 }
 
 class Event extends React.Component {
   render() {
     return (
-      <div>
+      <div class="event">
         <EventImage value={"https://res.cloudinary.com/wework/image/upload/c_fill,h_460,w_460/v1507739294/production/event/photo/1a6bce00-90cf-0135-57ba-4676e91ae0c9.jpg"}/>
-        <div>
-          <h5>December 5th * 12:30 - 14:00</h5>
-          <a href="https://members.wework.com/events/join-us-for-a-holiday-pop-up-with-shop-archipelago-56588"><h2>Join Us for a Holiday Pop-Up with Shop-Archipelago</h2></a>
-          <h5>Tower 49</h5>
+        <div class="textDiv">
+          <h1>Join Us for a Holiday Pop-Up with Shop-Archipelago</h1>          
+          <div class="timedate">
+            <div class="date">
+              <h2>December 5th * 12:30 - 14:00</h2>
+            </div>
+            <div class="time">
+              <h2>Tower 49</h2>
+            </div>
+          </div>
+          <div>Shop-Archipelago offers a growing family of labels from the vast archipelago of Indonesia and is empowering talented and creative craftspeople to access a global online marketplace. Their hand-picked selections consist of contemporary accessories that are highly intricate, made with exquisite, lasting materials and always hand finished. 
+30% discount to all WeWork members during the pop up!</div>
         </div>
       </div>
     );
